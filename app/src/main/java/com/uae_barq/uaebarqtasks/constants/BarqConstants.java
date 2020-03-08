6d1d88877affbff1a10b1e912244b83de9ea42cd
ahmedmohammed201300 <wowrar1234@gmail.com>
2020-03-08 16:28:44 +0200

    testingFirebaseRealtimeDatabase

---
+++ app/src/main/java/com/uae_barq/uaebarqtasks/constants/BarqConstants.java
@@ -0,0 +1,8 @@
+package com.uae_barq.uaebarqtasks.constants;
+
+public final class BarqConstants {
+
+    public static final String Status = "Status";
+
+
+}
--- app/src/main/java/com/uae_barq/uaebarqtasks/main/MainActivity.java
+++ app/src/main/java/com/uae_barq/uaebarqtasks/main/MainActivity.java
@@ -2,15 +2,25 @@ package com.uae_barq.uaebarqtasks.main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
 import com.uae_barq.uaebarqtasks.R;
+import com.uae_barq.uaebarqtasks.constants.BarqConstants;
 import com.uae_barq.uaebarqtasks.task_dynamic.TaskDynamicActivity;
 import com.uae_barq.uaebarqtasks.task_speed.TaskSpeedActivity;
+
+import java.util.HashMap;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -24,6 +34,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        testingFirebaseRealtimeDatabase();
+
+
+    }
+
+    private void testingFirebaseRealtimeDatabase() {
+        FirebaseDatabase.getInstance().getReference()
+                .setValue(new HashMap<String, Object>()
+                        .put(BarqConstants.Status, true))
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, R.string.connected, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, R.string.failed, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
     }
 
     @OnClick({R.id.btnStartTaskDynamic, R.id.btnStartTaskSpeed})
