026a01c2f27689ec695072769b8dcfb249fa7c6a
ahmedmohamed2030074 <wowrar1234@gmail.com>
2020-03-10 22:55:44 +0200

    AndroidJUnit4ClassRunner.

---
+++ app/src/androidTest/java/com/uae_barq/uaebarqtasks/main/DynamicLinkActivityTest.java
@@ -0,0 +1,16 @@
+package com.uae_barq.uaebarqtasks.main;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class DynamicLinkActivityTest {
+
+    @Test
+    public void testMainActivityContainerView() {
+
+
+    }
+}
