4e1e22a82a98e2735afc3e0c8b34bf0957e5b21e
ahmedmohamed2030074 <wowrar1234@gmail.com>
2020-03-10 23:00:12 +0200

    AndroidJUnit4ClassRunner.

--- app/src/androidTest/java/com/uae_barq/uaebarqtasks/main/DynamicLinkActivityTest.java
+++ app/src/androidTest/java/com/uae_barq/uaebarqtasks/main/DynamicLinkActivityTest.java
@@ -1,16 +1,36 @@
 package com.uae_barq.uaebarqtasks.main;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import com.uae_barq.uaebarqtasks.R;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class DynamicLinkActivityTest {
 
     @Test
-    public void testMainActivityContainerView() {
+    public void testMainViewContainer() {
+        ActivityScenario activityScenario = ActivityScenario.launch(DynamicLinkActivity.class);
 
+        onView(withId(R.id.main_container)).check(matches(isDisplayed()));
 
     }
+
+    @Test
+    public void testVisibilityOfCardAndChildViews() {
+
+        ActivityScenario activityScenario = ActivityScenario.launch(DynamicLinkActivity.class);
+
+        onView(withId())
+
+    }
+
 }
